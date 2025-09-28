a247f1a5cd1f243e579ff39b6bcef093388ed1c9
psion <psion@192.168.1.139>
2025-09-28 12:32:36 -0400

    testing file paths

--- Books/PlayerCore/Ancestries/Android.js
+++ Books/PlayerCore/Ancestries/Android.js
@@ -1,4 +1,4 @@
-import {Ancestry} from "../../../ConstructorClasses/Ancestry.js";
+import {Ancestry} from "/ConstructorClasses/Ancestry.js";
 
 //constructor(name, traits, hitPoints, size, baseSpeed, boosts, flaw, languages, vision, special, description)
 
@@ -12,6 +12,8 @@ export let android = new Ancestry(
     ["Cha"],
     ["Common"],
     "Low-Light Vision",
-    [{"Constructed": "Androids are immune to disease and sleep effects. They don’t need to eat or breathe, but they can ingest food and drink if they choose. They are not immune to poison, and they must still drink water or other fluids to avoid dehydration. Androids don’t suffer from fatigue due to lack of sleep and are immune to the fatigued condition from lack of rest. They still need 8 hours of downtime for daily preparations."}],
+    [
+		{"Constructed": "Androids are immune to disease and sleep effects. They don’t need to eat or breathe, but they can ingest food and drink if they choose. They are not immune to poison, and they must still drink water or other fluids to avoid dehydration. Androids don’t suffer from fatigue due to lack of sleep and are immune to the fatigued condition from lack of rest. They still need 8 hours of downtime for daily preparations."}
+	],
     "Androids are artificial beings created to resemble humans. They combine biological tissue with technological circuitry, making them resilient and efficient. While their features are often unnervingly perfect, subtle mechanical elements reveal their constructed nature."
 );
--- Books/PlayerCore/Ancestries/Kasatha.js
+++ Books/PlayerCore/Ancestries/Kasatha.js
@@ -1,4 +1,4 @@
-import {Ancestry} from "../../../ConstructorClasses/Ancestry.js";
+import {Ancestry} from "/ConstructorClasses/Ancestry.js";
 
 //constructor(name, traits, hitPoints, size, baseSpeed, boosts, flaw, languages, vision, special, description)
 
@@ -12,6 +12,8 @@ export let kasatha = new Ancestry(
 	["Cha"],
 	["Common", "Kasatha"],
 	"Normal",
-	[{"Four-Armed" : "You have four arms, which allows you to wield and hold up to four hands' worth of weapons and equipment. At any time, one pair of hands is designated as your active hands. You can change this designation from one pair of hands to another by taking the Switch Hands action action, which is a single action. You can wield items only with your active hands."}],
+	[
+		{"Four-Armed" : "You have four arms, which allows you to wield and hold up to four hands' worth of weapons and equipment. At any time, one pair of hands is designated as your active hands. You can change this designation from one pair of hands to another by taking the Switch Hands action action, which is a single action. You can wield items only with your active hands."}
+	],
 	"Kasathas are humanoids with gray skin, four arms, and pure black eyes. They have an elongated skull that extends behind their neck, further accentuating their tall frames. After evolving to survive migrations on a massive world, many kasathas have wiry bodies with thick musculature that makes them strong despite their lanky appearance."
 );
