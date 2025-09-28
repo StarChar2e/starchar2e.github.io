5e9dc861566ff4a0a21130850506de03b53be30d
psion <psion@192.168.1.139>
2025-09-28 11:08:55 -0400

    path change

---
+++ Books/Compiled/Ancestries.js
@@ -0,0 +1,9 @@
+import {playerCoreAncestries} from "../PlayerCore/Ancestries/Ancestries.js";
+import {galaxyGuideAncestries} from "../GalaxyGuide/Ancestries/_Ancestries.js";
+
+let mergedAncestries =  new Map([
+	...playerCoreAncestries, 
+	...galaxyGuideAncestries
+]);
+
+export let ancestries = new Map([...mergedAncestries.entries()].sort());
