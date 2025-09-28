14f1e6a81f3b64b7baf553cfa4f41615e61e7d0f
psion <psion@192.168.1.139>
2025-09-28 11:09:47 -0400

    deleted resource

--- Books/_Compiled/Ancestries.js
+++
@@ -1,9 +0,0 @@
-import {playerCoreAncestries} from "../PlayerCore/Ancestries/Ancestries.js";
-import {galaxyGuideAncestries} from "../GalaxyGuide/Ancestries/_Ancestries.js";
-
-let mergedAncestries =  new Map([
-	...playerCoreAncestries, 
-	...galaxyGuideAncestries
-]);
-
-export let ancestries = new Map([...mergedAncestries.entries()].sort());
