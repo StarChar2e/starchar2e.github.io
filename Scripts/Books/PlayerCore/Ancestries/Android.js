9eb7ee4484df72f1bccd2f9f19b094ad8e2cee30
psion <psion@192.168.1.139>
2025-09-29 15:02:47 -0400

    semicolon

--- Scripts/Books/PlayerCore/Ancestries/Android.js
+++ Scripts/Books/PlayerCore/Ancestries/Android.js
@@ -14,6 +14,6 @@ let ancestryInformation = {
 		"Constructed": "Androids are immune to disease and sleep effects. They don’t need to eat or breathe, but they can ingest food and drink if they choose. They are not immune to poison, and they must still drink water or other fluids to avoid dehydration. Androids don’t suffer from fatigue due to lack of sleep and are immune to the fatigued condition from lack of rest. They still need 8 hours of downtime for daily preparations."
 	},
 	description : 	"Androids are artificial beings created to resemble humans. They combine biological tissue with technological circuitry, making them resilient and efficient. While their features are often unnervingly perfect, subtle mechanical elements reveal their constructed nature."
-}
+};
 
 export let android = new Ancestry(ancestryInformation);
