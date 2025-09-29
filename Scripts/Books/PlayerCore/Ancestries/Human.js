4d759578ff6231802c9891c9c378c7cbfe7a5d22
mariellemejia <mariellemejia@192.168.100.12>
2025-09-29 13:23:13 -0600

    changed capitalized letters

--- Scripts/Books/PlayerCore/Ancestries/Human.js
+++ Scripts/Books/PlayerCore/Ancestries/Human.js
@@ -2,18 +2,18 @@ import {Ancestry} from "/Scripts/ConstructorClasses/Ancestry.js";
 
 // name; traits; hitPoints; size; baseSpeed; boosts; flaw; optionalFlaws; optionalBoost; languages; vision; special; description;
 
-export let Human = new Ancestry();
+export let human = new Ancestry();
 
-Human.setValueOf("name", "Human");
-Human.setValueOf("traits", ["Human", "Humanoid"]);
-Human.setValueOf("hitPoints", 8);
-Human.setValueOf("size", "Medium");
-Human.setValueOf("baseSpeed", 25);
-Human.setValueOf("boosts", ["Two free attribute boosts"]);
-Human.setValueOf("flaw", []);
-Human.setValueOf("languages", "Common");
-Human.setValueOf("vision", "Normal");
-Human.setValueOf("special", []);
-Human.setValueOf("description",
+human.setValueOf("name", "Human");
+human.setValueOf("traits", ["Human", "Humanoid"]);
+human.setValueOf("hitPoints", 8);
+human.setValueOf("size", "Medium");
+human.setValueOf("baseSpeed", 25);
+human.setValueOf("boosts", ["Free", "Free"]);
+human.setValueOf("flaw", []);
+human.setValueOf("languages", "Common");
+human.setValueOf("vision", "Normal");
+human.setValueOf("special", []);
+human.setValueOf("description",
   "Humans are known for their ability to adapt and thrive in the most challenging situations, including the mysterious loss of their home world, Golarion. Ambitious, creative, and endlessly curious, humans maintain a robust presence in the galaxy, often taking on positions of leadership or acting at the forefront of exploration. Humans can be found almost anywhere, doing almost anything.."
 );
--- Scripts/Books/PlayerCore/Ancestries/Skittermander.js
+++ Scripts/Books/PlayerCore/Ancestries/Skittermander.js
@@ -2,20 +2,20 @@ import {Ancestry} from "/Scripts/ConstructorClasses/Ancestry.js";
 
 // name; traits; hitPoints; size; baseSpeed; boosts; flaw; optionalFlaws; optionalBoost; languages; vision; special; description;
 
-export let Skittermander = new Ancestry();
+export let skittermander = new Ancestry();
 
-Skittermander.setValueOf("name", "Skittermander");
-Skittermander.setValueOf("traits", ["Humanoid","Skittermander"]);
-Skittermander.setValueOf("hitPoints", 6);
-Skittermander.setValueOf("size", "Small");
-Skittermander.setValueOf("baseSpeed", 25);
-Skittermander.setValueOf("boosts", ["Dex", "Cha", "Free"]);
-Skittermander.setValueOf("flaw", "Wis");
-Skittermander.setValueOf("languages", ["Common", "Vesk"]);
-Skittermander.setValueOf("vision", "Low-light");
-Skittermander.setValueOf("special", {
+skittermander.setValueOf("name", "Skittermander");
+skittermander.setValueOf("traits", ["Humanoid","Skittermander"]);
+skittermander.setValueOf("hitPoints", 6);
+skittermander.setValueOf("size", "Small");
+skittermander.setValueOf("baseSpeed", 25);
+skittermander.setValueOf("boosts", ["Dex", "Cha", "Free"]);
+skittermander.setValueOf("flaw", "Wis");
+skittermander.setValueOf("languages", ["Common", "Vesk"]);
+skittermander.setValueOf("vision", "Low-light");
+skittermander.setValueOf("special", {
   "Six-Armed" : "You have six arms, which allows you to wield and hold up to six hands' worth of weapons and equipment. At any time, one pair of hands is designated as your active hands. You can change this designation from one pair of hands to another by taking the Switch Hands action, which is a single action. You can wield items only with your active hands.."
 });
-Skittermander.setValueOf("description",
+skittermander.setValueOf("description",
   "Skittermanders are energetic, six-armed humanoids with brightly colored fur. Skittermanders accomplish tasks with chaotic enthusiasm and typically enjoy teamwork, defying even their would-be conquerors through collaboration."
 );
