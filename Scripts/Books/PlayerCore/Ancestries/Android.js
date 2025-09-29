2bafda029669c88dfb7d42186da35b52b6aa69a2
psion <psion@192.168.1.139>
2025-09-29 19:18:34 -0400

    variable name corrected

--- Scripts/Books/PlayerCore/Ancestries/Android.js
+++ Scripts/Books/PlayerCore/Ancestries/Android.js
@@ -2,18 +2,18 @@ import {Ancestry} from "/Scripts/ConstructorClasses/Ancestry.js";
 
 export let android = new Ancestry(ancestryInformation);
 
-kasatha.setValueOf("name", "Android");
-kasatha.setValueOf("traits", ["Humanoid", "Android"]);
-kasatha.setValueOf("hitPoints", 8);
-kasatha.setValueOf("size", "Medium");
-kasatha.setValueOf("baseSpeed", "25");
-kasatha.setValueOf("boosts", ["Dex", "Int", "Free"]);
-kasatha.setValueOf("flaw", ["Cha"]);
-kasatha.setValueOf("languages", ["Common"]);
-kasatha.setValueOf("vision", "Low-Light Vision");
-kasatha.setValueOf("special", {
+android.setValueOf("name", "Android");
+android.setValueOf("traits", ["Humanoid", "Android"]);
+android.setValueOf("hitPoints", 8);
+android.setValueOf("size", "Medium");
+android.setValueOf("baseSpeed", "25");
+android.setValueOf("boosts", ["Dex", "Int", "Free"]);
+android.setValueOf("flaw", ["Cha"]);
+android.setValueOf("languages", ["Common"]);
+android.setValueOf("vision", "Low-Light Vision");
+android.setValueOf("special", {
 	"Constructed" : {
 		description: "Androids are immune to disease and sleep effects. They don’t need to eat or breathe, but they can ingest food and drink if they choose. They are not immune to poison, and they must still drink water or other fluids to avoid dehydration. Androids don’t suffer from fatigue due to lack of sleep and are immune to the fatigued condition from lack of rest. They still need 8 hours of downtime for daily preparations."
 	}
 });
-kasatha.setValueOf("description", "Androids are artificial beings created to resemble humans. They combine biological tissue with technological circuitry, making them resilient and efficient. While their features are often unnervingly perfect, subtle mechanical elements reveal their constructed nature.");
+android.setValueOf("description", "Androids are artificial beings created to resemble humans. They combine biological tissue with technological circuitry, making them resilient and efficient. While their features are often unnervingly perfect, subtle mechanical elements reveal their constructed nature.");
