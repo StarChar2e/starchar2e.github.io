15c4b758a6a1138f731af7884aee3041ce069e1e
psion <psion@192.168.1.139>
2025-09-28 23:25:52 -0400

    testing custom setter and getters

--- ConstructorClasses/Ancestry.js
+++ ConstructorClasses/Ancestry.js
@@ -1,30 +1,32 @@
 export class Ancestry {
-	#name;
-	#traits;
-	#hitPoints;
-	#size;
-	#baseSpeed;
-	#boosts;
-	#flaw;
-	#optionalFlaws;
-	#optionalBoost;
-	#languages;
-	#vision;
-	#special;
-	#description;
+	name;
+	traits;
+	hitPoints;
+	size;
+	baseSpeed;
+	boosts;
+	flaw;
+	optionalFlaws;
+	optionalBoost;
+	languages;
+	vision;
+	special;
+	description;
 	
 	constructor(obj) {
-		this.#name = obj.name || "";
-		this.#traits = obj.traits || [];
-		this.#hitPoints = obj.hitPoints || 8;
-		this.#size = obj.size || "Medium";
-		this.#baseSpeed = obj.baseSpeed || 25;
-		this.#boosts = obj.boosts || ["Free", "Free"];
-		this.#flaw = obj.flaw || [];
-		this.#languages = obj.languages || ["Common"];
-		this.#vision = obj.vision || "Normal"
-		this.#special = obj.special || [{}];
-		this.#description = obj.description || "";
+		this.name = obj.name || "";
+		this.traits = obj.traits || [];
+		this.hitPoints = obj.hitPoints || 8;
+		this.size = obj.size || "Medium";
+		this.baseSpeed = obj.baseSpeed || 25;
+		this.boosts = obj.boosts || ["Free", "Free"];
+		this.flaw = obj.flaw || [];
+		this.optionalFlaws = obj.optionalFlaws || [];
+		this.optionalBoost = obj.optionalBoost || [];
+		this.languages = obj.languages || ["Common"];
+		this.vision = obj.vision || "Normal"
+		this.special = obj.special || [{}];
+		this.description = obj.description || "";
 	}
 	
 	getValueOf(name){
