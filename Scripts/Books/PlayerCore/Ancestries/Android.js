ccec63b30409b7601a94b176a30a5a74c672d6c2
psion <psion@192.168.1.139>
2025-09-29 19:18:03 -0400

    androids made consistent

--- Scripts/Books/PlayerCore/Ancestries/Android.js
+++ Scripts/Books/PlayerCore/Ancestries/Android.js
@@ -1,21 +1,19 @@
 import {Ancestry} from "/Scripts/ConstructorClasses/Ancestry.js";
 
-let ancestryInformation = {
-	name : "Android",
-	traits : ["Humanoid", "Android"],
-	hitPoints : 8,
-	size : "Medium",
-	baseSpeed : 25,
-	boosts : ["Dex", "Int", "Free"],
-	flaw : ["Cha"],
-	languages : ["Common"],
-	vision : "Low-Light Vision",
-	special : {
-		"Constructed": {
-			desription : "Androids are immune to disease and sleep effects. They don’t need to eat or breathe, but they can ingest food and drink if they choose. They are not immune to poison, and they must still drink water or other fluids to avoid dehydration. Androids don’t suffer from fatigue due to lack of sleep and are immune to the fatigued condition from lack of rest. They still need 8 hours of downtime for daily preparations."
-		}
-	},
-	description : "Androids are artificial beings created to resemble humans. They combine biological tissue with technological circuitry, making them resilient and efficient. While their features are often unnervingly perfect, subtle mechanical elements reveal their constructed nature."
-};
-
 export let android = new Ancestry(ancestryInformation);
+
+kasatha.setValueOf("name", "Android");
+kasatha.setValueOf("traits", ["Humanoid", "Android"]);
+kasatha.setValueOf("hitPoints", 8);
+kasatha.setValueOf("size", "Medium");
+kasatha.setValueOf("baseSpeed", "25");
+kasatha.setValueOf("boosts", ["Dex", "Int", "Free"]);
+kasatha.setValueOf("flaw", ["Cha"]);
+kasatha.setValueOf("languages", ["Common"]);
+kasatha.setValueOf("vision", "Low-Light Vision");
+kasatha.setValueOf("special", {
+	"Constructed" : {
+		description: "Androids are immune to disease and sleep effects. They don’t need to eat or breathe, but they can ingest food and drink if they choose. They are not immune to poison, and they must still drink water or other fluids to avoid dehydration. Androids don’t suffer from fatigue due to lack of sleep and are immune to the fatigued condition from lack of rest. They still need 8 hours of downtime for daily preparations."
+	}
+});
+kasatha.setValueOf("description", "Androids are artificial beings created to resemble humans. They combine biological tissue with technological circuitry, making them resilient and efficient. While their features are often unnervingly perfect, subtle mechanical elements reveal their constructed nature.");
